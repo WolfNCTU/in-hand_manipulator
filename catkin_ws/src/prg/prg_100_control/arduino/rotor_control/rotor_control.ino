f457bd3ef567b30d09034931339c509f993f3912
WolfNCTU <andy11259@yahoo.com.tw>
2019-10-22 09:32:12 +0800

    add serial node

--- catkin_ws/src/prg/prg_100_control/arduino/rotor_control/rotor_control.ino
+++ catkin_ws/src/prg/prg_100_control/arduino/rotor_control/rotor_control.ino
@@ -8,33 +8,55 @@ PRG-100 Rotor Controller
 #define OUTPUT_PIN_1 2
 #define OUTPUT_PIN_2 3
 
+int state = 0;
+
 void setup(){
   Serial.begin(115200);
   pinMode(OUTPUT_PIN_1, OUTPUT);
   pinMode(OUTPUT_PIN_2, OUTPUT);
+  Serial.print("Start Connecting\n");
 }
 
-void loop(){
-  cw_turn(5000);
-  stop_turn(1000);
-  ccw_turn(5000);
-  stop_turn(1000);
+void loop()
+{
+  state = Serial.parseInt();
+  // morot command loop
+  if(Serial.available()) motor_status();
 }
 
-void stop_turn(int time){
-  digitalWrite(OUTPUT_PIN_1, LOW);
-  digitalWrite(OUTPUT_PIN_2, LOW);
-  delay(time);
+void motor_status()
+{
+  stop_turn();
+  cw_turn();
+  ccw_turn();
 }
 
-void cw_turn(int time){
-  digitalWrite(OUTPUT_PIN_1, HIGH);
-  digitalWrite(OUTPUT_PIN_2, LOW);
-  delay(time);
+void stop_turn()
+{
+  if(state == 0)
+  {
+    Serial.print("Stop\n");
+    digitalWrite(OUTPUT_PIN_1, LOW);
+    digitalWrite(OUTPUT_PIN_2, LOW);
+  }
 }
 
-void ccw_turn(int time){
-  digitalWrite(OUTPUT_PIN_1, LOW);
-  digitalWrite(OUTPUT_PIN_2, HIGH);
-  delay(time);
+void cw_turn()
+{
+  if(state == 1)
+  {  
+    Serial.print("CW Rotate\n");
+    digitalWrite(OUTPUT_PIN_1, HIGH);
+    digitalWrite(OUTPUT_PIN_2, LOW);    
+  }
+}
+
+void ccw_turn()
+{
+  if(state == 2)
+  {
+    Serial.print("CCW Rotate\n");
+    digitalWrite(OUTPUT_PIN_1, LOW);
+    digitalWrite(OUTPUT_PIN_2, HIGH);
+  }
 }
